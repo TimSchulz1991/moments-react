42060cce95828a01a72dd0c16a2380ce4f4dafa2
Tim Schulz <tim@getbej.com>
2022-05-23 06:26:48 +0000

    Add feature to edit profile info, username and pw

--- src/App.js
+++ src/App.js
@@ -11,6 +11,9 @@ import PostsPage from "./pages/posts/PostsPage";
 import PostEditForm from "./pages/posts/PostEditForm";
 import { useCurrentUser } from "./context/CurrentUserContext";
 import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 function App() {
     const currentUser = useCurrentUser();
@@ -70,6 +73,21 @@ function App() {
                         exact
                         path="/profiles/:id/"
                         render={() => <ProfilePage />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/username"
+                        render={() => <UsernameForm />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit/password"
+                        render={() => <UserPasswordForm />}
+                    />
+                    <Route
+                        exact
+                        path="/profiles/:id/edit"
+                        render={() => <ProfileEditForm />}
                     />
                     <Route render={() => <p>Page not found!</p>} />
                 </Switch>
--- src/pages/profiles/UsernameForm.js
+++ src/pages/profiles/UsernameForm.js
@@ -10,84 +10,88 @@ import Row from "react-bootstrap/Row";
 import { useHistory, useParams } from "react-router-dom";
 import { axiosRes } from "../../api/axiosDefaults";
 import {
-  useCurrentUser,
-  useSetCurrentUser,
-} from "../../contexts/CurrentUserContext";
+    useCurrentUser,
+    useSetCurrentUser,
+} from "../../context/CurrentUserContext";
 
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 const UsernameForm = () => {
-  const [username, setUsername] = useState("");
-  const [errors, setErrors] = useState({});
+    const [username, setUsername] = useState("");
+    const [errors, setErrors] = useState({});
 
-  const history = useHistory();
-  const { id } = useParams();
+    const history = useHistory();
+    const { id } = useParams();
 
-  const currentUser = useCurrentUser();
-  const setCurrentUser = useSetCurrentUser();
+    const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
 
-  useEffect(() => {
-    if (currentUser?.profile_id?.toString() === id) {
-      setUsername(currentUser.username);
-    } else {
-      history.push("/");
-    }
-  }, [currentUser, history, id]);
+    useEffect(() => {
+        // since the profile id is an integer, and the param id is a string, we need to convert the integer into a string using the toString() method before we can check that they are equal.
+        if (currentUser?.profile_id?.toString() === id) {
+            setUsername(currentUser.username);
+        } else {
+            history.push("/");
+        }
+        // We’re relying on the currentUser value here, which is fetched asynchronously on mount. So if you were to refresh the page, you’d get redirected to home, because the currentUser is initially null. It takes a moment for the API response to come back saying that you’re actually logged in.
+    }, [currentUser, history, id]);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      await axiosRes.put("/dj-rest-auth/user/", {
-        username,
-      });
-      setCurrentUser((prevUser) => ({
-        ...prevUser,
-        username,
-      }));
-      history.goBack();
-    } catch (err) {
-      console.log(err);
-      setErrors(err.response?.data);
-    }
-  };
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axiosRes.put("/dj-rest-auth/user/", {
+                username,
+            });
+            setCurrentUser((prevUser) => ({
+                ...prevUser,
+                username,
+            }));
+            history.goBack();
+        } catch (err) {
+            console.log(err);
+            setErrors(err.response?.data);
+        }
+    };
 
-  return (
-    <Row>
-      <Col className="py-2 mx-auto text-center" md={6}>
-        <Container className={appStyles.Content}>
-          <Form onSubmit={handleSubmit} className="my-2">
-            <Form.Group>
-              <Form.Label>Change username</Form.Label>
-              <Form.Control
-                placeholder="username"
-                type="text"
-                value={username}
-                onChange={(event) => setUsername(event.target.value)}
-              />
-            </Form.Group>
-            {errors?.username?.map((message, idx) => (
-              <Alert key={idx} variant="warning">
-                {message}
-              </Alert>
-            ))}
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Blue}`}
-              onClick={() => history.goBack()}
-            >
-              cancel
-            </Button>
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Blue}`}
-              type="submit"
-            >
-              save
-            </Button>
-          </Form>
-        </Container>
-      </Col>
-    </Row>
-  );
+    return (
+        <Row>
+            <Col className="py-2 mx-auto text-center" md={6}>
+                <Container className={appStyles.Content}>
+                    <Form onSubmit={handleSubmit} className="my-2">
+                        <Form.Group>
+                            <Form.Label>Change username</Form.Label>
+                            <Form.Control
+                                placeholder="username"
+                                type="text"
+                                value={username}
+                                onChange={(event) =>
+                                    setUsername(event.target.value)
+                                }
+                            />
+                        </Form.Group>
+                        {errors?.username?.map((message, idx) => (
+                            <Alert key={idx} variant="warning">
+                                {message}
+                            </Alert>
+                        ))}
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Blue}`}
+                            onClick={() => history.goBack()}
+                        >
+                            cancel
+                        </Button>
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Blue}`}
+                            type="submit"
+                        >
+                            save
+                        </Button>
+                    </Form>
+                </Container>
+            </Col>
+        </Row>
+    );
 };
 
 export default UsernameForm;
